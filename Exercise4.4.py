c5917f125645a76863233548ec6501136a006908
gloxiinia <rachelanastasia.w@gmail.com>
2023-03-09 21:29:43 +0700

    added functions

--- Exercise4.4.py
+++ Exercise4.4.py
@@ -1,13 +1,37 @@
-from __future__ import division, print_function
+import numpy as np
 
-from numpy import linspace, sqrt
-from math import pi
-N = 100000000
-h = 2/N
-x = linspace(-1,1,N)
-y = sqrt(1 - x**2)
+print("Calculating the integral of root of (1-x^2) from x=-1 to x=1 \n\n")
 
-I = h*sum(y)*2
-delta = I - pi
-print('The value of integral is I = {}'.format(I))
-print('delta = {}'.format(delta))
+#Declaring the variables
+
+def calculateIntegral (N_value):
+    N = N_value
+    h = 2/N
+    x = np.linspace(-1,1,N)
+    y = np.sqrt(1 - x**2)
+
+    I = h*sum(y)*2
+    delta = I - np.pi
+    print(f"With an N value of {N_value}: ")
+    print('I = {}'.format(I))
+    print('delta = {}'.format(delta))
+
+print("Without 1 second constraint:")
+print("--------------------------------------\n")
+calculateIntegral(100)
+print("--------------------------------------\n")
+calculateIntegral(1000)
+print("--------------------------------------\n")
+calculateIntegral(10000)
+print("--------------------------------------\n")
+calculateIntegral(100000)
+print("--------------------------------------\n")
+calculateIntegral(1000000)
+print("--------------------------------------\n")
+calculateIntegral(10000000)
+print("--------------------------------------\n")
+calculateIntegral(100000000)
+print("--------------------------------------\n")
+
+   
+               
