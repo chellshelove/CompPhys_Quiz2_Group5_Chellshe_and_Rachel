3e42a8160fde53ae5fb607dcc1b8472839d4de87
chellshelove <chellshelove733@gmail.com>
2023-03-09 19:08:19 +0700

    testing

---
+++ .history/Exercise4.4_20230309190605.py
@@ -0,0 +1,13 @@
+from __future__ import division, print_function
+
+from numpy import linspace, sqrt
+from math import pi
+N = 100000000
+h = 2/N
+x = linspace(-1,1,N)
+y = sqrt(1 - x**2)
+
+I = h*sum(y)*2
+delta = I - pi
+print('The value of integral is I = {}'.format(I))
+print('delta = {}'.format(delta))
